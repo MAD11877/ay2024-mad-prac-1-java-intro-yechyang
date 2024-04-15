c04ddc63daebe5fb0860e7d3cbafe247ca24e545
yechyang <148125893+yechyang@users.noreply.github.com>
2024-04-15 15:21:29 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,7 +26,32 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
-    
+      Scanner in = new Scanner(System.in);
+      System.out.print("");
+      int num = in.nextInt();
+      int[] list = new int[num];
+      
+      for(int i = 0; i < num; i++) {
+          System.out.print("Enter an integer: ");
+          list[i] = in.nextInt();
+      }
+      
+      int[] frequency = new int[101];
+      
+      for(int i = 0; i < num; i++) {
+          frequency[list[i]]++;
+      }
+      
+      int mode = 0;
+      int maxFrequency = 0;
+      for(int i = 0; i < frequency.length; i++) {
+          if(frequency[i] > maxFrequency) {
+              maxFrequency = frequency[i];
+              mode = i;
+          }
+      }
+      
+      System.out.println("The mode is: " + mode);
+
   }
 }
